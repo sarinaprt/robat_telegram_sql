ac916b6b9d3e0c6f63f962666a7983dbf86d1ce2
sarina ebrahimi <sarinae914@gmail.com>
2025-10-15 10:33:04 +0330

    done

--- DDL.py
+++ DDL.py
@@ -3,162 +3,207 @@ from mysql.connector import connection
 
 
 def customer_add(USERNAME,CHAT_ID,NAME):
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("INSERT INTO CUSTOMER(USERNAME,CHAT_ID,NAME)VALUES(%s,%s,%s)",(USERNAME,CHAT_ID,NAME))
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("INSERT INTO CUSTOMER(USERNAME,CHAT_ID,NAME)VALUES(%s,%s,%s)",(USERNAME,CHAT_ID,NAME))
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e :
+        print(f"problem in ad_custom {e}⚠️ ")
 
 def chek_customer(CHAT_ID):
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("SELECT CHAT_ID FROM customer where CHAT_ID=%s",(CHAT_ID,))
-    chat_id=cur.fetchone()
-    cur.close()
-    conn.close()
-    if chat_id:
-        print(chat_id)
-        return chat_id[0]
-    else:
-        return None
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("SELECT CHAT_ID FROM customer where CHAT_ID=%s",(CHAT_ID,))
+        chat_id=cur.fetchone()
+        cur.close()
+        conn.close()
+        if chat_id:
+            print(chat_id)
+            return chat_id[0]
+        else:
+            return None
+    except Exception as e :
+        print(f"problem in check_custom {e}⚠️ ")
+
     
 def find_user_id(CHAT_ID):
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("SELECT USER_ID FROM CUSTOMER WHERE CHAT_ID=%s",(CHAT_ID,))
-    user_id=cur.fetchone()
-    cur.close()
-    conn.close()
-    if user_id:
-        return user_id[0]
-    else:
-        return None
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("SELECT USER_ID FROM CUSTOMER WHERE CHAT_ID=%s",(CHAT_ID,))
+        user_id=cur.fetchone()
+        cur.close()
+        conn.close()
+        if user_id:
+            return user_id[0]
+        else:
+            return None
+    except Exception as e :
+        print(f"problem in find_user {e}⚠️ ")
 
 def insert_book(title ,gener,author,file_url):
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("INSERT INTO BOOKS(title ,gener,author,file_url)VALUES (%s,%s,%s,%s)",(title ,gener,author,file_url))
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("INSERT INTO BOOKS(title ,gener,author,file_url)VALUES (%s,%s,%s,%s)",(title ,gener,author,file_url))
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e :
+        print(f"problem in ad_book {e}⚠️ ")
+
 
 def search_books(date):
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("SELECT author,GROUP_CONCAT(title) as titles FROM books WHERE gener=%s GROUP BY author" ,(date,))
-    book=cur.fetchall()
-    cur.close()
-    conn.close()
-    if book:
-        return book
-    else:
-        return None
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("SELECT author,GROUP_CONCAT(title) as titles FROM books WHERE gener=%s GROUP BY author" ,(date,))
+        book=cur.fetchall()
+        cur.close()
+        conn.close()
+        if book:
+            return book
+        else:
+            return None
+    except Exception as e:
+        print(f"problem in search_book {e}⚠️ ")
+
 
 def file_url_book(author,title):
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("SELECT file_url FROM books WHERE author=%s AND title=%s",(author,title))
-    url=cur.fetchall()
-    cur.close()
-    conn.close()
-    if url:
-        return url[0][0]
-    else:
-        return None
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("SELECT file_url FROM books WHERE author=%s AND title=%s",(author,title))
+        url=cur.fetchall()
+        cur.close()
+        conn.close()
+        if url:
+            return url[0][0]
+        else:
+            return None
+    except Exception as e :
+        print(f"problem in book_url {e}⚠️ ")
+
     
 def random_books():
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("SELECT title,author,file_url FROM books ")
-    books=cur.fetchall()
-    cur.close()
-    conn.close()
-    if books:
-        print(books)
-        return books
-    else:
-        return None
-    
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("SELECT title,author,file_url FROM books ")
+        books=cur.fetchall()
+        cur.close()
+        conn.close()
+        if books:
+            print(books)
+            return books
+        else:
+            return None
+    except Exception as e :
+        print(f"problem in random_book {e}⚠️ ")
 
 def insert_theater(title,pic_url,text,Duration,price,actors,stock):
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("INSERT INTO ticket(title,pic_url,text,Duration,price,actors,stock)VALUES(%s,%s,%s,%s,%s,%s,%s)",(title,pic_url,text,Duration,price,actors,stock))
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("INSERT INTO ticket(title,pic_url,text,Duration,price,actors,stock)VALUES(%s,%s,%s,%s,%s,%s,%s)",(title,pic_url,text,Duration,price,actors,stock))
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e :
+        print(f"problem in ad_theater {e}⚠️ ")
+
                 
 def theater(gener):
-    config={"user":"root","password":"belive_god1527","host":"localhost","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("SELECT title,text,Duration,price,actors,pic_url,tk_id,stock FROM ticket WHERE gener=%s",(gener,))
-    url=cur.fetchall()
-    cur.close()
-    conn.close() 
-    if url:
-        return url
-    else:
-        return None
+    try:
+        config={"user":"root","password":"belive_god1527","host":"localhost","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("SELECT title,text,Duration,price,actors,pic_url,tk_id,stock FROM ticket WHERE gener=%s",(gener,))
+        url=cur.fetchall()
+        cur.close()
+        conn.close() 
+        if url:
+            return url
+        else:
+            return None
+    except Exception as e:
+        print(f"problem in select_theater {e}⚠️ ")
+
 
 def add_orders(user_id,ITEM_TYPE,quantity):
-    config={"user":"root","password":"belive_god1527","host":"localhost","database":"shop_bot"} 
-    conn=connection.MySQLConnection(**config) 
-    cur=conn.cursor()
-    cur.execute("INSERT INTO orders(user_id,ITEM_TYPE,quantity)VALUES(%s,%s,%s)",(user_id,ITEM_TYPE,quantity))
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        config={"user":"root","password":"belive_god1527","host":"localhost","database":"shop_bot"} 
+        conn=connection.MySQLConnection(**config) 
+        cur=conn.cursor()
+        cur.execute("INSERT INTO orders(user_id,ITEM_TYPE,quantity)VALUES(%s,%s,%s)",(user_id,ITEM_TYPE,quantity))
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f"problem in ad_orders {e}⚠️ ")
 
 def REPORT():
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("""SELECT c.CHAT_ID, c.USERNAME
-                    FROM customer c
-                    INNER JOIN orders o ON c.user_id = o.user_id
-                    GROUP BY c.user_id
-                    HAVING DATE(MAX(o.creat_at)) = DATE(NOW());
-                    """)
-    active=cur.fetchall()
-    cur.close()
-    conn.close()
-    if active:
-        print(active)
-        return active
-    else:
-        return None
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("""SELECT c.CHAT_ID, c.USERNAME
+                        FROM customer c
+                        INNER JOIN orders o ON c.user_id = o.user_id
+                        GROUP BY c.user_id
+                        HAVING DATE(MAX(o.creat_at)) = DATE(NOW());
+                        """)
+        active=cur.fetchall()
+        cur.close()
+        conn.close()
+        if active:
+            print(active)
+            return active
+        else:
+            return None
+    except Exception as e:
+        print(f"problem in report {e}⚠️ ")
 
 def update_quantity(quantity,tk_id):
-    config={"user":"root","password":"belive_god1527","host":"localhost","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("update ticket set stock=stock-%s where tk_id=%s",(quantity,tk_id))
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        config={"user":"root","password":"belive_god1527","host":"localhost","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("update ticket set stock=stock-%s where tk_id=%s",(quantity,tk_id))
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e :
+        print(f"problem in ad_custom {e}⚠️ ")
 
 def check_stock(tk_id):
-    config={"user":"root","password":"belive_god1527","host":"localhost","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("select stock from ticket where tk_id=%s",(tk_id,))
-    stock=cur.fetchone()
-    cur.close()
-    conn.close()
-    if stock:
-        return stock[0]
-    else:
-        return None
+    try:
+        config={"user":"root","password":"belive_god1527","host":"localhost","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("select stock from ticket where tk_id=%s",(tk_id,))
+        stock=cur.fetchone()
+        cur.close()
+        conn.close()
+        if stock:
+            return stock[0]
+        else:
+            return None
+    except Exception as e :
+        print(f"problem in check_stock {e}⚠️ ")
+
 
 if __name__=="__main__":
     search_books()
--- DML.py
+++ DML.py
@@ -1,87 +1,105 @@
 from mysql.connector import connection
 
 def database_exsist(database):
-    config={"user":"root","host":"localhost","password":"belive_god1527"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute(f"DROP DATABASE IF EXISTS  {database}")
-    cur.execute(f"CREATE DATABASE {database}")
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute(f"DROP DATABASE IF EXISTS  {database}")
+        cur.execute(f"CREATE DATABASE {database}")
+        conn.commit()
+        cur.close()
+        conn.close()   
+    except Exception as e :
+        print(f"problem in database_exist {e}⚠️ ")
+ 
 
 def customers():
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("""CREATE TABLE CUSTOMER(
-                USERNAME  NVARCHAR(50) ,
-                USER_ID   INT    AUTO_INCREMENT PRIMARY KEY,
-                CHAT_ID   VARCHAR(100)UNIQUE NOT NULL,
-                NAME      VARCHAR(50) NOT NULL ,
-                CREATE_AT DATETIME DEFAULT CURRENT_TIMESTAMP,
-                STATUS    DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("""CREATE TABLE CUSTOMER(
+                    USERNAME  NVARCHAR(50) ,
+                    USER_ID   INT    AUTO_INCREMENT PRIMARY KEY,
+                    CHAT_ID   VARCHAR(100)UNIQUE NOT NULL,
+                    NAME      VARCHAR(50) NOT NULL ,
+                    CREATE_AT DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    STATUS    DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  
+        ) """)
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e :
+        print(f"problem in table_custom {e}⚠️ ")
 
-    ) """)
-    conn.commit()
-    cur.close()
-    conn.close()
 
 
 def book_info():
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("""CREATE TABLE BOOKS(
-                bok_id    INT AUTO_INCREMENT PRIMARY KEY,
-                title     NVARCHAR(50) NOT NULL ,
-                gener     ENUM("Psychology","Novel","History","religious","Educational") NOT NULL,
-                author    NVARCHAR(50),
-                file_url  VARCHAR(225) NOT NULL,
-                creat_at  DATETIME DEFAULT CURRENT_TIMESTAMP
-    )""")
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("""CREATE TABLE BOOKS(
+                    bok_id    INT AUTO_INCREMENT PRIMARY KEY,
+                    title     NVARCHAR(50) NOT NULL ,
+                    gener     ENUM("Psychology","Novel","History","religious","Educational") NOT NULL,
+                    author    NVARCHAR(50),
+                    file_url  VARCHAR(225) NOT NULL,
+                    creat_at  DATETIME DEFAULT CURRENT_TIMESTAMP
+        )""")
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f"problem in table_book {e}⚠️ ")
+
 
 
 
 def theater_ticket():
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("""CREATE TABLE ticket(
-                tk_id     INT AUTO_INCREMENT PRIMARY KEY,
-                title     NVARCHAR(50) NOT NULL,
-                gener     ENUM("Comedy","Drama","Children","Musical","Historical") NOT NULL,
-                pic_url   VARCHAR(225) NOT NULL,
-                text      NVARCHAR(100),
-                Duration  TIME,
-                price     DECIMAL(7,3) NOT NULL,
-                actors    NVARCHAR(50),
-                stock     int default 0 ,
-                creat_at  DATETIME DEFAULT CURRENT_TIMESTAMP
-    )""")
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("""CREATE TABLE ticket(
+                    tk_id     INT AUTO_INCREMENT PRIMARY KEY,
+                    title     NVARCHAR(50) NOT NULL,
+                    gener     ENUM("Comedy","Drama","Children","Musical","Historical") NOT NULL,
+                    pic_url   VARCHAR(225) NOT NULL,
+                    text      NVARCHAR(100),
+                    Duration  TIME,
+                    price     DECIMAL(7,3) NOT NULL,
+                    actors    NVARCHAR(50),
+                    stock     int default 0 ,
+                    creat_at  DATETIME DEFAULT CURRENT_TIMESTAMP
+        )""")
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f"problem in table_theater_ticket {e}⚠️ ")
 
 
 def orders():
-    config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
-    conn=connection.MySQLConnection(**config)
-    cur=conn.cursor()
-    cur.execute("""CREATE TABLE orders(
-                ord_id    INT AUTO_INCREMENT PRIMARY KEY,
-                user_id   INT NOT NULL,
-                ITEM_TYPE ENUM("E-Books","theaters"),
-                quantity  INT DEFAULT 0,
-                creat_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN   KEY (user_id) REFERENCES CUSTOMER(USER_ID)
-    )""")
-    conn.commit()
-    cur.close()
-    conn.close()
+    try:
+        config={"user":"root","host":"localhost","password":"belive_god1527","database":"shop_bot"}
+        conn=connection.MySQLConnection(**config)
+        cur=conn.cursor()
+        cur.execute("""CREATE TABLE orders(
+                    ord_id    INT AUTO_INCREMENT PRIMARY KEY,
+                    user_id   INT NOT NULL,
+                    ITEM_TYPE ENUM("E-Books","theaters"),
+                    quantity  INT DEFAULT 0,
+                    creat_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN   KEY (user_id) REFERENCES CUSTOMER(USER_ID)
+        )""")
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f"problem in table_orders {e}⚠️ ")
+
 
 
 
